02ae06468ac1481b0925ca1df998ec9b6516197a
홍준성 <ghdwnstjd13@gmail.com>
2022-09-03 18:21:50 +0900

    git init > New Project > dependencies
    - spring-boot-starter-data-jpa
    - spring-boot-starter-web
    - lombok
    - h2database

---
+++ README.md
@@ -0,0 +1,7 @@
+# JPA 프로그래밍
+
+1. dependencies
+   - spring-boot-starter-data-jpa
+   - spring-boot-starter-web
+   - lombok
+   - h2database
---
+++ bookmanager/src/test/java/spring/study/bookmanager/controller/HelloWorldControllerTest.java
@@ -0,0 +1,32 @@
+package spring.study.bookmanager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld() throws Exception {
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/hello-world")
+        ).andExpect(
+            MockMvcResultMatchers.status().isOk()
+        ).andExpect(
+            MockMvcResultMatchers.content().string("hello-world")
+        ).andDo(
+            MockMvcResultHandlers.print()
+        );
+    }
+}
